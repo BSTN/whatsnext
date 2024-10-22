e4bc313183e609cc0be2e8f3fbabf2867883f386
BSTN <basten@bstn.nl>
2024-10-22 15:57:45 +0100

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/content', 'nuxt-security', '@nuxtjs/i18n'],
+  modules: ['@nuxt/content', 'nuxt-security', '@nuxtjs/i18n', '@nuxt/image'],
 
   routeRules: {
     '/': { prerender: true }
