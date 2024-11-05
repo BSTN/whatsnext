d94be4c668fabb316f752cd52bfb735187c84233
BSTN <basten@bstn.nl>
2024-11-05 15:47:01 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
     sources: {
       content: {
         driver: 'fs',
-        base: resolve(__dirname, 'repos/content')
+        base: resolve(__dirname, 'repos/content/')
       }
     }
   },
