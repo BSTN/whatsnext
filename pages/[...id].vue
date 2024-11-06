3bc71d04be1f300ff92c04b60d805fc3e1d566c6
BSTN <basten@bstn.nl>
2024-11-06 10:11:33 +0000

    update

--- pages/[...id].vue
+++ pages/[...id].vue
@@ -16,6 +16,7 @@ const { locale } = useI18n()
 const pad = computed(() => {
   const langcode = locale.value
   const p = route.path === '/' ? '/index' : route.path
+  console.log(p + '.' + langcode)
   return p + '.' + langcode
 })
 
